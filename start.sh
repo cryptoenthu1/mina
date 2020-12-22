3aa9db15cefbe45121d4ce88b98510b3fe496ebd
cryptoenthu1 <67186962+cryptoenthu1@users.noreply.github.com>
2020-12-22 10:23:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,9 +14,9 @@ echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc
 sleep 5
 sudo apt-get update
 sleep 5
-sudo apt-get install -y curl mina-testnet-postake-medium-curves=0.1.0-beta1-56b97f1 --allow-downgrades
+sudo apt-get install -y curl mina-testnet-postake-medium-curves=0.1.1+-temporary-qa-staging-ci-wait-cf2b767
 sleep 5
-wget -O ~/peers.txt https://raw.githubusercontent.com/MinaProtocol/coda-automation/master/terraform/testnets/turbo-pickles/peers.txt
+wget -O ~/peers.txt https://raw.githubusercontent.com/MinaProtocol/coda-automation/bug-bounty-net/terraform/testnets/testworld/peers.txt
 sleep 5
 cp .profile .profile_bkp
 sleep 5
@@ -39,7 +39,7 @@ sleep 3
 source .profile
 sleep 3
 source .profile
-sudo docker run --name mina -d --restart always -p 8301-8305:8301-8305 -v $(pwd)/peers.txt:/root/peers.txt -v $(pwd)/keys:/root/keys:ro -v $(pwd)/.coda-config:/root/.coda-config minaprotocol/mina-daemon-baked:4.1-turbo-pickles-forked-mina56b97f1-autoeaa923f daemon -peer-list-file $HOME/peers.txt -block-producer-key $KEYPATH -block-producer-password $CODA_PRIVKEY_PASS -insecure-rest-server -file-log-level Debug -log-level Info
+sudo docker run --name mina -d --restart always -p 8301-8305:8301-8305 -v $(pwd)/peers.txt:/root/peers.txt -v $(pwd)/keys:/root/keys:ro -v $(pwd)/.coda-config:/root/.coda-config gcr.io/o1labs-192920/coda-daemon-baked:0.1.1-temporary-qa-staging-ci-wait-cf2b767-testworld-f226fed daemon -peer-list-file $HOME/peers.txt -block-producer-key $KEYPATH -block-producer-password $CODA_PRIVKEY_PASS -insecure-rest-server -file-log-level Debug -log-level Info
 sleep 5
 sudo docker exec -it mina coda client status
 sleep 10
