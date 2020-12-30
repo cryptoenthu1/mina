4cdfaeaf5c7a5c283838e5b4d4ab9bebc6e228f0
cryptoenthu1 <67186962+cryptoenthu1@users.noreply.github.com>
2020-12-30 13:50:17 +0530

    Create trans.sh

---
+++ trans.sh
@@ -0,0 +1,72 @@
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qijScmzJgaYsXZqw4qbFHiBXpbKNXRNNkaHRjrUTRwcVZ1YbSfF5 -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qmf8idnmU8gxKroXC6RjhxwvTBn57GQYt1EgL9pFuJYtsJT4ee8G -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qmxCUx7aJaKN7d5axw9rNEtcFctzd2rjKjQfiQW7YBXG7rSPtCNx -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qoTkJM3KH7YaoArz7iLL89FyBPNX1Avc7eUpmQqANVcvwevGduWL -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qj4L8DEoAQKdokEyVZU7cgAmGpRg5zQy4PnXkDtaUEueNuapbRLT -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qqdcBgFMikJCU8ucVN9euwEcf44G4WutJiusTSMJExUDjLpx4LgG -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qjZcbuQGvTaxiN6NaMpGNGo5ee6cPcDJttVTn4i6dQyRdsZPC4e6 -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qp9Qi7drTD287M5tnAsVLjdSFbtnFZmruoKhSEaTkGWpDiweS4HX -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qmut36jDpAtYtfyHNhuCurGy7mtAooTtFCSL7EbjbrfPbS4CSzHv -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qk6xUSBbAeFQkSUvLfsxmkqfVfEJXJn2ZbxzWNgdvb1TbggKR1FB -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qretDZqaqPbXcK8xczoXDmF1FaZPYyQ3beEovUATCRj6M49EiAMn -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qs1h1FmU6m2vottoip8zJ9wmt4o22qp5Arz1bMFx5XQBsF4tEUwY -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qmgqVDXPUSN2B25AxPz9BvpcbC4WyYjxJzqz5C6G4TsUDGgp8aXs -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qmJQzmeWoVpnpcm8Z6C1T1tq8e6kJdMk1MDj4KpJnxMSFoJS54Y7 -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qiWtkLQb1kszoHVqU8VpfugTEJiUeKzvjgDG3Q92sWTkaqfixL8x -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qjYENB7ifEroyF9ob9oFj11sQiSZpJ3tewDKiNhdUT9NAVAheVE9 -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qr56QrR4meyJtDpQtevNUFCmRySpDJUbx4fDSNB2agNg2BUw4aZZ -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qjUR3gdZvMBUMcxzuXAP4y63YFNY1J1ep3MNcNS1UiyzmDEvwi3a -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qqFzxykCgsezjFGWnxdjFErJb6ZA6qwyx3vfJz5D8aRfQjrJdCTn -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qjEHvphMTbqdVUxKw7EbgqshEHBWmGFWfSMbUTPZCm1mey8HwmNu -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qiabSCfwNSrZSsD7tKfKEr1bPRfJPr6FwZuYuHKzQZpByCtbAYDY -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qos4Qvzso58zU7FpzZ3HXy7yyXPTDivSjG2UEwpQJgVAwtoRyvy4 -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qmtxHKX8HbAUBwgExjesQaFthQAsDsb65mhvSYjPsnMackQBnPzp -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qotvLZ58RTgx3EVEk6rMxvipwEouVU2EUJ5nZUKpfZaqGkABgXWY -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qpW8kfCzjco5tH6a73FV3cCbj259T1zpUcvuY8DG7Ma8k8hyDGgM -fee 0.1 -amount 0.01
+
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qpMH1eW9XwcDzPrMi4uD2tCv7YXrSGYccgs4LdDkMfgfUgtLzRGL -fee 0.1 -amount 0.01
+
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qoyTXNSkQY5pWEYg7FYNgDPGzmi6Ek8b5vNtQdFQ7VdbC6oZo2Wb -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qkUw79PxjGEtBk1koeBnddV6u461zZgGhCdZtUCPvrGs9xpWKqeB -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qmEwkjBDhWtkhc6f2haENkBJysxy7yniDd2NsnyfWB3rm5mG6bfN -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qrqLBXU5aPLt3jcYZbZGX39C7n1gtpueZmxFt46GEHeRofo7FF7N -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qmMpdzJAuTPC4cc8k9XNRxWExx3Wv2fJP7cYs8sRFLVpFwo4vzXA -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qiY1Yj4prLUMEGDhEf5T6qBaN6g9GD87xePWbVApd3xsbo72zuoR -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qm14hrZB2dkkpDrw6BWKTF8AuB26jcG5JqeLovdUJnKezA5aKm1A -fee 0.1 -amount 0.01
+
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qkwYMJYq89diRrnupXcVb6DU17BsDxTxE8M1wA7L4V5Msm6jiXCR -fee 0.1 -amount 0.01
+
+sudo docker exec -it mina coda client send-payment -sender $MINA_PUBLIC_KEY -receiver B62qnui8Zz5wFU6UGJ7cygJWaSfcG6eAB5rHTGTgEjb6aaNk2PojY9i -fee 0.1 -amount 0.01
