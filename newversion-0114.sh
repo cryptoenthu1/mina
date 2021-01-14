70e08637b8ab46059b6ff98001ca006da0509a57
cryptoenthu1 <67186962+cryptoenthu1@users.noreply.github.com>
2021-01-14 11:40:47 +0530

    Update newversion-0114.sh

--- newversion-0114.sh
+++ newversion-0114.sh
@@ -1,3 +1,5 @@
+docker rm -f mina
+sleep 2
 sudo apt-get remove -y mina-testnet-postake-medium-curves
 sleep 2
 echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
@@ -9,6 +11,7 @@ sleep 2
 wget -O ~/peers.txt https://raw.githubusercontent.com/MinaProtocol/coda-automation/bug-bounty-net/terraform/testnets/testworld/peers.txt
 sleep 2
 coda version
+sleep 2
 sudo docker run --name mina -d --restart always -p 8301-8305:8301-8305 -v $(pwd)/peers.txt:/root/peers.txt -v $(pwd)/keys:/root/keys:ro -v $(pwd)/.coda-config:/root/.coda-config minaprotocol/mina-daemon-baked:0.2.4-16ebdd6-testworld-16ebdd6 daemon -peer-list-file $HOME/peers.txt -block-producer-key $KEYPATH -block-producer-password $MINA_PRIVKEY_PASS -insecure-rest-server -file-log-level Info -log-level Info -super-catchup
 sleep 2
 sudo docker exec -it mina coda client status
