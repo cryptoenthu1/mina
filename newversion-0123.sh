265df6e818e4e5de944f0a8852f2d6abc412334d
cryptoenthu1 <67186962+cryptoenthu1@users.noreply.github.com>
2021-01-23 12:02:30 +0530

    Create newversion-0123.sh

---
+++ newversion-0123.sh
@@ -0,0 +1,21 @@
+docker rm -f mina
+sleep 2
+rm -rf $HOME/.coda-config
+sudo apt-get remove -y mina-testnet-postake-medium-curves
+sleep 2
+echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
+sleep 2
+sudo apt-get update
+sleep 2
+sudo apt-get install -y curl unzip mina-testnet-postake-medium-curves=0.2.9-a940247
+sleep 2
+wget -O ~/peers.txt https://raw.githubusercontent.com/MinaProtocol/coda-automation/bug-bounty-net/terraform/testnets/testworld/peers.txt
+sleep 2
+coda version
+sleep 2
+source .profile
+sudo docker run --name mina -d --restart always -p 8301-8305:8301-8305 -v $(pwd)/peers.txt:/root/peers.txt -v $(pwd)/keys:/root/keys:ro -v $(pwd)/.coda-config:/root/.coda-config minaprotocol/mina-daemon-baked:0.2.9-a940247-testworld-a940247 daemon -peer-list-file $HOME/peers.txt -block-producer-key $KEYPATH -block-producer-password $MINA_PRIVKEY_PASS -insecure-rest-server -file-log-level Info -log-level Info -super-catchup
+sleep 2
+sudo docker exec -it mina coda client status
+sleep 2
+sudo docker logs --tail 1000 mina -f
